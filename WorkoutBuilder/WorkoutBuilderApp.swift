8d7bbbe57256c9e699dd10031518ae1aec69ee77
anhduong275 <84537455+anhduong275@users.noreply.github.com>
2023-06-18 10:15:23 -0700

    Initial Commit

---
+++ WorkoutBuilder/WorkoutBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutBuilderApp.swift
+//  WorkoutBuilder
+//
+//  Created by Ha Anh on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
